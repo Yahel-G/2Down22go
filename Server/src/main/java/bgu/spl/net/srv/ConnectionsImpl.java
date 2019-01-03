eae2469f33cd28660976d5421a50a24fab4c4ebe
Yahel-G <171717+Yahel-G@users.noreply.github.com>
2019-01-03 15:43:41 +0200

    z

---
+++ Server/src/main/java/bgu/spl/net/srv/ConnectionsImpl.java
@@ -0,0 +1,44 @@
+package bgu.spl.net.srv;
+
+import bgu.spl.net.api.bidi.Connections;
+
+import java.util.concurrent.ConcurrentHashMap;
+
+/**
+ * Created by Yahel on 22/12/2018.
+ */
+public class ConnectionsImpl<T> implements Connections<T> {
+//todo: think if functions should be protected/private/public
+
+    private ConcurrentHashMap<Integer, bgu.spl.net.srv.bidi.ConnectionHandler<T>> handlersMap; // todo: concurrent?
+
+    public ConnectionsImpl(){
+        handlersMap = new ConcurrentHashMap<>();
+    }
+
+    public boolean send(int connectionId, T msg){
+        if(handlersMap.containsKey(connectionId)){
+            if(msg != null){ // maybe still send msg even if it's null? maybe it can't be null?
+                handlersMap.get(connectionId).send(msg);
+                return true;
+            } else return true;
+        } else {// else, still send the msg to the client when he logs in?
+            return false;
+        }
+    }
+
+    public void broadcast(T msg){
+        for(Integer userId : handlersMap.keySet()) {
+            handlersMap.get(userId).send(msg);
+        }
+    }
+
+    public void disconnect(int connectionId){
+
+        handlersMap.remove(connectionId);
+    }
+
+    public ConcurrentHashMap<Integer, bgu.spl.net.srv.bidi.ConnectionHandler<T>> getHandlersMap() {
+        return handlersMap;
+    }
+}
